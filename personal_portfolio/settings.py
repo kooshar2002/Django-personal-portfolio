abf7042af49565b8d6ce7a5b6cd501a8df50a229
koosha rahimi <rkoosha46@gmail.com>
2021-07-23 16:49:14 +0430

    requirements commit

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -128,3 +128,8 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from personal_portfolio.local_settings import *
+except ImportError:
+    print('Looks like there is no file. You must be on production')
