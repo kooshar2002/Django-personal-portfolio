c30e7b5e36080ffcb04aa62d651b4ca37a9dee6e
koosha rahimi <rkoosha46@gmail.com>
2021-07-23 12:05:14 +0430

    last commit

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,4 +3,9 @@ from .models import project
 
 def home(request):
     projects = project.objects.all()
-    return render(request, 'portfolio\home.html', {'project': projects})
+    return render(request, 'portfolio/home.html', {'project': projects})
+
+
+def about(request):
+
+    return render(request, 'portfolio/about.html')
